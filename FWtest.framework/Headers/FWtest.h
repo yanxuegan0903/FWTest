c56243b4b8515395bc9fdb0f56bf5d4deb034d55
yanxuegan <yanxuegan@ciyun.com>
2020-01-14 16:19:44 +0800

    origin

---
+++ FWtest.framework/Headers/FWtest.h
@@ -0,0 +1,20 @@
+//
+//  FWtest.h
+//  FWtest
+//
+//  Created by 颜学赶 on 2020/1/14.
+//  Copyright © 2020 颜学赶. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FWtest.
+FOUNDATION_EXPORT double FWtestVersionNumber;
+
+//! Project version string for FWtest.
+FOUNDATION_EXPORT const unsigned char FWtestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FWtest/PublicHeader.h>
+
+
+#import "FrameworkManager.h"
---
+++ FWtest.framework/Headers/FrameworkManager.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkManager.h
+//  FrameworkTest
+//
+//  Created by 颜学赶 on 2020/1/13.
+//  Copyright © 2020 颜学赶. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FrameworkManager : NSObject
+
++ (void)pushVC:(UINavigationController *)navi;
+
+@end
+
+NS_ASSUME_NONNULL_END
